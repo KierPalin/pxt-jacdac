235572c727c377cb71b5e5c4547b99371f79e204
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-07-09 17:53:25 +0100

    experimenting with display/server.ts

---
+++ display/server.ts
@@ -0,0 +1,30 @@
+import JDButtonEvent = jacdac.ButtonEvent
+import MkcdButtonEvent = ButtonEvent
+
+namespace jacdac {
+    export class DisplayServer extends jacdac.SensorServer {
+        constructor(
+            // public readonly button: Button,
+            options?: jacdac.SensorServerOptions
+        ) {
+            super(jacdac.SRV_DISPLAY, options)
+            // this.button.onEvent(MkcdButtonEvent.Down, () =>
+            //     this.sendEvent(JDButtonEvent.Down)
+            // )
+            // this.button.onEvent(MkcdButtonEvent.Up, () =>
+            //     this.sendEvent(JDButtonEvent.Up)
+            // )
+            // this.button.onEvent(<number>DAL.DEVICE_BUTTON_EVT_HOLD, () =>
+            //     this.sendEvent(JDButtonEvent.Hold)
+            // )
+        }
+
+        public hello(): string {
+            // const pressed = this.button.isPressed()
+            // return jacdac.jdpack(jacdac.ButtonRegPack.Pressure, [
+            //     pressed ? 1 : 0,
+            // ])
+            return "hello";
+        }
+    }
+}
